db9d1ce275a02f8f1acac214a46e938f40139fa8
sonofajulian <sonofajulian@gmail.com>
2024-02-25 20:47:02 -0700

    Defined and added functions to TestBoard - Julian Reyes

--- src/experiment/TestBoard.java
+++ src/experiment/TestBoard.java
@@ -1,5 +1,45 @@
 package experiment;
 
+import java.util.HashSet;
+import java.util.Set;
+
+
 public class TestBoard {
+	/** 
+	 * Default Constructor: Initializes Test Board
+	 */	
+	public TestBoard() {
+		super();
+	}
+	
+	/** 
+	 * calcTargets: calculates legal targets for a move from startCell of length pathlength.
+	 * 
+	 */
+	
+	public void calcTargets( TestBoardCell startCell, int pathlength) {
+		return;
+	}
+	
+	/** 
+	 *  getCell: returns the cell from the board at row, col.
+	 * 
+	 */
+	
+	public TestBoardCell getCell(int row, int col) {
+	TestBoardCell returnCell = new TestBoardCell(row, col);
+		return returnCell;	
+	}
+	
+	/** 
+	 *   getTargets: gets the targets last created by calcTargets().
+	 * 
+	 */
+	
+	public Set<TestBoardCell> getTargets() {
+		Set<TestBoardCell> returnSet = new HashSet <TestBoardCell>();
+		return returnSet;
+		
+	}
 
 }
