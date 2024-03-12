be1342583b687b9db277ea7733507f77185a0dc2
mathew grossman <mathewgrossman@mines.edu>
2024-03-11 21:29:02 -0600

    fixed misspelled enum access in BoardCell.java.

--- src/clueGame/BoardCell.java
+++ src/clueGame/BoardCell.java
@@ -24,7 +24,7 @@ public class BoardCell {
 	public BoardCell(int row, int col) {
 		this.row = row;
 		this.col = col;
-		doorDirection = doorDirection.NONE;
+		doorDirection = DoorDirection.NONE;
 		roomLabel = false;
 		roomCenter = false;
 		isOccupied = false;
