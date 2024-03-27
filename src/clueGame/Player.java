ec07bd37fa8b76d08d4f9d37eb8e4bbe00ad6739
sonofajulian <sonofajulian@gmail.com>
2024-03-26 20:32:57 -0600

    Added player classes

---
+++ src/clueGame/Player.java
@@ -0,0 +1,15 @@
+package clueGame;
+
+import java.awt.Color;
+
+public abstract class Player {
+	private String name;
+	private Color color;
+	private int row;
+	private int column;
+	
+	public void updateHand(Card card) {
+		return;
+	}
+
+}
